ac376b90f901692bbe62e5bbbcbf21638f27b2a7
Kayla-Ard <kaylard@yahoo.com>
2024-08-12 20:26:19 -0600

    tenant home page updates

--- Maintenance_App/settings.py
+++ Maintenance_App/settings.py
@@ -139,13 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / 'global_static'
+    BASE_DIR / 'app' / 'static',
 ]
+
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
