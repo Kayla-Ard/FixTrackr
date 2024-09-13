d0b68700eb6ca574523be47f3255a1b68cd4a0d6
Kayla-Ard <kaylard@yahoo.com>
2024-09-13 11:13:06 -0500

    added frontend URL to settings

--- Maintenance_App/settings.py
+++ Maintenance_App/settings.py
@@ -193,6 +193,7 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # frontend's URL local
+    "https://fixtrackr.vercel.app",  # your deployed frontend on Vercel
     
 ]
 
